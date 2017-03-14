c80d7ca1fa3dc84b701559958be7715042caf43d
SeeEun Cho <chwhse@gmail.com>
2017-03-14 21:25:30 +0900

    ..

--- src/main/java/donghun2/dao/DaoProduct.java
+++ src/main/java/donghun2/dao/DaoProduct.java
@@ -6,7 +6,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Vector;
 
+import coffee.dto.DtoCodeCoffee;
 import donghun2.dto.Product;
+import kr.or.dgit.it.coffee.jdbc.DBCon;
 
 
 public class DaoProduct implements Dao<Product> {
@@ -22,6 +24,9 @@ public class DaoProduct implements Dao<Product> {
 			pstmt = connection.prepareStatement(sql);
 			pstmt.setString(1, item.getCode());
 			pstmt.setString(2, item.getName());
+			pstmt.setInt(3, item.getSalePrice());
+			pstmt.setInt(4, item.getOrigiPrice());
+			
 			res = pstmt.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -38,14 +43,39 @@ public class DaoProduct implements Dao<Product> {
 
 	@Override
 	public Vector<Product> selectItemByAll() {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "select code,name,saleprice,origiprice from product";
+		//DataBase 연결(접속)
+		DBCon dbCon = new DBCon();
+		Connection connection = dbCon.getConnection();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		Vector<Product> list = new Vector<>();
+			try {
+				pstmt = connection.prepareStatement(sql);
+				rs = pstmt.executeQuery();
+				while(rs.next()){
+					list.add(getObject(rs));
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					rs.close();
+					pstmt.close();
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			return list;
 	}
 
 	@Override
 	public Product getObject(ResultSet rs) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		String code = rs.getString("code");
+		String name = rs.getString("name");
+		int saleprice = rs.getInt("saleprice");
+		int origiprice = rs.getInt("origiprice");
 	}
 
 	@Override
