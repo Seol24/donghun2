0eaabb9d37c6e01eb426407c22b0e03bfbe25c4c
SeeEun Cho <chwhse@gmail.com>
2017-03-14 21:13:44 +0900

    170314조시은

--- src/main/java/donghun2/dao/DaoProduct.java
+++ src/main/java/donghun2/dao/DaoProduct.java
@@ -1,5 +1,7 @@
 package donghun2.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Vector;
@@ -11,8 +13,27 @@ public class DaoProduct implements Dao<Product> {
 
 	@Override
 	public int insertItem(Product item) {
-		// TODO Auto-generated method stub
-		return 0;
+		String sql = "replace into product(code,name,saleprice,origiprice) VALUES (?,?,?,?)";
+		DBCon dbCon = new DBCon();
+		Connection connection = dbCon.getConnection();
+		PreparedStatement pstmt = null;
+		int res = -1;
+		try {
+			pstmt = connection.prepareStatement(sql);
+			pstmt.setString(1, item.getCode());
+			pstmt.setString(2, item.getName());
+			res = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				pstmt.close();
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return res;
 	}
 
 	@Override
--- src/main/java/donghuns2/view/ProductView.java
+++ src/main/java/donghuns2/view/ProductView.java
@@ -1,23 +1,33 @@
 package donghuns2.view;
 
+import java.awt.Component;
 import java.awt.EventQueue;
 import java.awt.GridLayout;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import donghun2.dao.DaoProduct;
+import donghun2.dto.Product;
 import donghun2.panel.ProductPanel;
+import erp_myframework.TextFiledPanel;
 
-public class ProductView extends JFrame {
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ProductView extends JFrame implements ActionListener {
 
 	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
+	private JButton btnSave;
+	private JButton btnDele;
+	private JButton btnSearch;
+	protected DaoProduct dao;
+	private ProductPanel pProduct;
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -30,10 +40,7 @@ public class ProductView extends JFrame {
 			}
 		});
 	}
-
-	/**
-	 * Create the frame.
-	 */
+	
 	public ProductView() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 322);
@@ -42,21 +49,87 @@ public class ProductView extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
 		
-		ProductPanel pProduct = new ProductPanel();
+		pProduct = new ProductPanel();
 		contentPane.add(pProduct);
 		
 		JPanel pBtn = new JPanel();
 		contentPane.add(pBtn);
 		pBtn.setLayout(new GridLayout(0, 3, 0, 0));
 		
-		JButton btnSave = new JButton("저장");
+		btnSave = new JButton("저장");
+		btnSave.addActionListener(this);
 		pBtn.add(btnSave);
 		
-		JButton btnDele = new JButton("삭제");
+		btnDele = new JButton("삭제");
+		btnDele.addActionListener(this);
 		pBtn.add(btnDele);
 		
-		JButton btnSearch = new JButton("검색");
+		btnSearch = new JButton("검색");
+		btnSearch.addActionListener(this);
 		pBtn.add(btnSearch);
 	}
+	
+	public void setDao(DaoProduct dao) {
+		this.dao = dao;
+	}
 
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnSearch) {
+			actionPerformedBtnSearch(e);
+		}
+		if (e.getSource() == btnDele) {
+			actionPerformedBtnDele(e);
+		}
+		if (e.getSource() == btnSave) {
+			actionPerformedBtnSave(e);
+		}
+	}
+	protected void actionPerformedBtnSave(ActionEvent e) {
+		if(isValidcheck()){
+			JOptionPane.showMessageDialog(null, "공백이 존재");
+			return;
+		}
+		Product item = getObject();
+		dao.insertItem(item);
+		JOptionPane.showMessageDialog(null, "추가됨");
+		clear();
+	}
+	private Product getObject() {
+		String code = pProduct.pCode.getTfValue();
+		String name = pProduct.pName.getTfValue();
+		int saleprice = Integer.parseInt(pProduct.pSalePrice.getTfValue());
+		int origiprice = Integer.parseInt(pProduct.pOrigiPrice.getTfValue());
+		return new Product(code, name, saleprice, origiprice);
+	}
+
+	protected void actionPerformedBtnDele(ActionEvent e) {
+		
+	}
+	protected void actionPerformedBtnSearch(ActionEvent e) {
+		
+	}
+	
+	private boolean isValidcheck() {
+		return isEmpty();
+	}
+
+	public void clear(){
+		pProduct.pCode.setTfValue("");
+		pProduct.pName.setTfValue("");
+		pProduct.pSalePrice.setTfValue("");
+		pProduct.pOrigiPrice.setTfValue("");
+		
+	}
+	public boolean isEmpty(){
+		for(Component c : getComponents()){
+			if(c instanceof TextFiledPanel){
+				TextFiledPanel tfp = (TextFiledPanel) c;
+				if(tfp.isEmpty()){
+					return true;
+				}
+
+			};
+		}
+		return false;
+	}
 }
