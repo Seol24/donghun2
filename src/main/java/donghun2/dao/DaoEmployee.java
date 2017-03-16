b00b58df94216c4e6554ac2dac6b934b380fe48b
jwSHIN <kimcio505@naver.com>
2017-03-16 16:41:18 +0900

    2017-03-16 JinWook's File

--- src/main/java/donghun2/dao/DaoEmployee.java
+++ src/main/java/donghun2/dao/DaoEmployee.java
@@ -1,50 +1,169 @@
 package donghun2.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Vector;
 
 import donghun2.dto.Employee;
-
+import donghun2.jdbc.DbCon;
 
 public class DaoEmployee implements Dao<Employee> {
+	private static DaoEmployee instance = new DaoEmployee();
+
+	public DaoEmployee() {}
+
+	public static DaoEmployee getInstance() {
+		return instance;
+	}
 
 	@Override
 	public int insertItem(Employee item) {
-		// TODO Auto-generated method stub
+		String sql = "insert into employee(code,name,grade) values(?,?,?)";
+
+		DbCon dbCon = new DbCon();
+		Connection connection = dbCon.getConnection();
+		PreparedStatement pstmt = null;
+		try {
+			pstmt = connection.prepareStatement(sql);
+			pstmt.setString(1, item.getCode());
+			pstmt.setString(2, item.getName());
+			pstmt.setString(3, item.getGrade());
+			int res = pstmt.executeUpdate();
+			System.out.println("insert : " + res);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				pstmt.close();
+				dbCon.Close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 		return 0;
 	}
 
 	@Override
 	public Vector<Employee> selectItemByAll() {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "select code,name,grade from employee";
+		Vector<Employee> list = null;
+
+		DbCon dbCon = new DbCon();
+		Connection connection = dbCon.getConnection();
+
+		// SQL 문장 완성
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			pstmt = connection.prepareStatement(sql);
+			// SQL 실행
+			rs = pstmt.executeQuery();
+			if (rs.next()) {// 하나 이상의 결과가 존재 할 경우
+				list = new Vector<>();
+				do {
+					list.add(getObject(rs));
+				} while (rs.next());
+			} else {// 하나도 없을 경우
+				list = new Vector<>();
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				rs.close();
+				pstmt.close();
+				dbCon.Close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+
+			}
+		}
+		return list;
 	}
 
 	@Override
 	public Employee getObject(ResultSet rs) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		String code = rs.getString("code");
+		String name = rs.getString("name");
+		String grade = rs.getString("grade");
+
+		return new Employee(code, name, grade);
 	}
 
 	@Override
-	public Employee selectItemByNo(String code) {
-		// TODO Auto-generated method stub
-		return null;
+	public Employee selectItemByNo(Employee item) {
+		String sql = "select name,grade from employee where code = ?";
+		DbCon dbCon = new DbCon();
+		Connection connection = dbCon.getConnection();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		Employee employee = null;
+		try {
+			pstmt = connection.prepareStatement(sql);
+			pstmt.setString(1, item.getCode());
+			System.out.println(pstmt);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				employee = getObject(rs);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return employee;
 	}
 
 	@Override
 	public int updateItem(Employee item) {
-		// TODO Auto-generated method stub
-		return 0;
+		String sql = "update employee set name=?,grade=? where code=?";
+		DbCon dbCon = new DbCon();
+		Connection connection = dbCon.getConnection();
+		PreparedStatement pstmt = null;
+		int res = -1;
+		try {
+			pstmt = connection.prepareStatement(sql);
+			pstmt.setString(1, item.getName());
+			pstmt.setString(2, item.getGrade());
+			pstmt.setString(3, item.getCode());
+			res = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				pstmt.close();
+				dbCon.Close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			dbCon.Close();
+		}
+		return res;
 	}
 
 	@Override
-	public int deleteItem(String code) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int deleteItem(Employee item) {
+		String sql = "delete from employee where code=?";
+		DbCon dbCon = new DbCon();
+		Connection connection = dbCon.getConnection();
+		PreparedStatement pstmt = null;
+		int res = -1;
+		try {
+			pstmt = connection.prepareStatement(sql);
+			pstmt.setString(1, item.getCode());
+			res = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				pstmt.close();
+				dbCon.Close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			dbCon.Close();
+		}
+		return res;
 	}
-	//화이팅
-//화이팅2	
-	//화이팅3
 }
---
+++ src/main/java/donghun2/jdbc/DbCon.java
@@ -0,0 +1,37 @@
+package donghun2.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbCon {
+	private String dbName = "donghun2";
+	private String url = "jdbc:mysql://localhost:3306/" + dbName;
+	private String user = "user_donghun2";
+	private String password = "rootroot";
+	private Connection connection;
+
+	public DbCon() {
+		try {
+			connection = DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			System.out.println("연결 실패 - 사용자 정보 및 드라이버를 찾을 수 없음");
+			e.printStackTrace();
+		}
+	}
+
+	public Connection getConnection() {
+		return connection;
+	}
+
+	public void Close() {
+		if (connection != null) {
+			try {
+				connection.close();
+				connection = null;
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
